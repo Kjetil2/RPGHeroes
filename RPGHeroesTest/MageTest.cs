371721d94567e7944ff2f3e3ddefb86262fd5e2a
Kjetil Rønhovde <kjetil.ronhovde@no.experis.com>
2023-01-19 21:33:16 +0100

    comment on test

--- RPGHeroesTest/MageTest.cs
+++ RPGHeroesTest/MageTest.cs
@@ -5,19 +5,25 @@ using Xunit;
 
 namespace RPGHeroesTest
 {
+    // Creating a test class
     public class MageTest
     {
         [Fact]
+        
+        //Creating a new test Methode
         public void MageLevelUp_AddoneLevel_ShouldReturnLevelPlussOne()
         
         {
             Mage mage = new Mage("Kjetil");
             
-            
+            // Arrange 
             HeroAttribute expected = new HeroAttribute(1, 1, 8);
 
+            //Act
             HeroAttribute actual = new HeroAttribute(1,1,8);
 
+
+            //Assert
             Assert.Equal(expected, actual);
             
         }
